624736d72a84f7cab43eefa5947fc61e7567d3bd
ArunMaurya <arunmaurya2500@gmail.com>
2021-01-16 19:33:42 +0530

    live data updation using timer(1,100).subscribe()

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription, timer } from 'rxjs';
 import { AdminDataCountService } from 'src/app/services/admin-data-count.service';
 
 @Component({
@@ -21,22 +21,28 @@ export class DashboardComponent implements OnInit , OnDestroy{
   
 
   ngOnInit(): void {
-   this.subscription.add( this.adminDataCountService.fetchNumberOfAllAppointments().subscribe(data=>{
-    this.total_appointments  = data['total_appointments']
-  }))
-  this.subscription.add( this.adminDataCountService.fetchNumberOfNewAppointments().subscribe(data=>{
-    this.new_appointments = data['new_appointments']
-  }))
-   
-  this.subscription.add( this.adminDataCountService.fetchNumberOfVerifiedUsers().subscribe(data=>{
-    this.verified_user = data['verified_users']
-  }))
-   this.subscription.add(this.adminDataCountService.fetchNumberOfTotalUsers().subscribe(data=>{
-    this.total_users = data['all_users']
-  }))
-    this.subscription.add(this.adminDataCountService.fetchNumberOfPendingAppointments().subscribe(data=>{
-      this.pending_case = data['pending_appointments']
-    }))
+    timer(0,100).subscribe((sub)=>{
+      this.subscription.add( this.adminDataCountService.fetchNumberOfAllAppointments().subscribe(data=>{
+        this.total_appointments  = data['total_appointments']
+      }))
+      
+    
+      this.subscription.add( this.adminDataCountService.fetchNumberOfNewAppointments().subscribe(data=>{
+        this.new_appointments = data['new_appointments']
+      }))
+       
+      this.subscription.add( this.adminDataCountService.fetchNumberOfVerifiedUsers().subscribe(data=>{
+        this.verified_user = data['verified_users']
+      }))
+       this.subscription.add(this.adminDataCountService.fetchNumberOfTotalUsers().subscribe(data=>{
+        this.total_users = data['all_users']
+      }))
+        this.subscription.add(this.adminDataCountService.fetchNumberOfPendingAppointments().subscribe(data=>{
+          this.pending_case = data['pending_appointments']
+        }))
+      
+    } )
+  
     
   }
 
