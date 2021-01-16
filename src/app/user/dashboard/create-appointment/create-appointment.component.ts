ee9f917764967b46414645125f74dafad0c474f9
ArunMaurya <arunmaurya2500@gmail.com>
2021-01-16 15:55:47 +0530

    appointment file upload implemented

--- src/app/user/dashboard/create-appointment/create-appointment.component.ts
+++ src/app/user/dashboard/create-appointment/create-appointment.component.ts
@@ -1,5 +1,11 @@
 import { stringify } from "@angular/compiler/src/util";
-import { Component, ComponentFactoryResolver, OnInit, ViewChild, ViewContainerRef } from "@angular/core";
+import {
+  Component,
+  ComponentFactoryResolver,
+  OnInit,
+  ViewChild,
+  ViewContainerRef,
+} from "@angular/core";
 declare var $: any;
 import {
   FormBuilder,
@@ -8,7 +14,7 @@ import {
   Validators,
 } from "@angular/forms";
 import { Router } from "@angular/router";
-declare var $ : any
+declare var $: any;
 
 import { AppointmentService } from "../../../services/appointment.service";
 
@@ -23,17 +29,16 @@ export class CreateAppointmentComponent implements OnInit {
   modalMsg: string;
   msg: string;
   title: string;
-  showModal =  false;
+  showModal = false;
   product: any;
+  image: any;
 
   ngOnInit(): void {}
- 
-  
+
   constructor(
     private fb: FormBuilder,
     private appointmentService: AppointmentService,
-    private router: Router,
-   
+    private router: Router
   ) {
     const currentDate = this.formatted_date();
     this.appointForm = this.fb.group({
@@ -56,37 +61,59 @@ export class CreateAppointmentComponent implements OnInit {
   }
 
   create_Appointment(appointForm) {
-    const appointmentData = {
-      title: appointForm.value.problem_Title,
-      description: appointForm.value.problem_Description,
-      image: appointForm.value.problem_Image,
-      pick_up_date: appointForm.value.pickup_Date,
-      pick_up_time:
-        appointForm.value.pickup_Time + " " + appointForm.value.meridian,
-      address: appointForm.value.user_Address,
-      phone: appointForm.value.user_Phone,
-    };
-    console.log(appointmentData)
+    const fd = new FormData();
+    
+    fd.append('title', appointForm.get('problem_Title').value)
+    fd.append('description', appointForm.get('problem_Description').value)
+    fd.append('pick_up_date', appointForm.get('pickup_Date').value)
+    fd.append('pick_up_time', appointForm.get('pickup_Time').value+ " " + appointForm.value.meridian)
+    fd.append('phone', appointForm.get('user_Phone').value)
+    fd.append('address', appointForm.get('user_Address').value)
+    fd.append('image', appointForm.get('problem_Image').value)
 
+    // const appointmentData = {
+    //   title: appointForm.value.problem_Title,
+    //   description: appointForm.value.problem_Description,
+     
+    //   // image: appointForm.value.problem_Image,
+    //   pick_up_date: appointForm.value.pickup_Date,
+    //   pick_up_time:
+    //     appointForm.value.pickup_Time + " " + appointForm.value.meridian,
+    //   address: appointForm.value.user_Address,
+    //   phone: appointForm.value.user_Phone,
+    // };
+    // console.log(appointmentData);
+    fd.forEach((value, key) => {                                                                                                                             
+      console.log("key (%s): value (%s)", key, value);                                                                                                           
+   })
+    console.log(fd)
+    
     this.appointmentService
-      .postAppointment(appointmentData)
+      .postAppointment(fd)
       .subscribe((data) => {
         this.product = data;
-        if (this.product) {          
+        console.log(this.product)
+        if (this.product) {
           this.title = "Appointment Fixed";
           this.msg =
             "Your Appointment is successfuly Fixed - Appointment Number is " +
-             this.product.appointment_number;
-              
-            // $("#modal").modal("show")
-            // $("#modal").modal({backdrop: 'static', keyboard: false})
-            $('#modal').modal({
-              backdrop: 'static',
-              keyboard: false, 
-              show: true
-      }); 
+            this.product.appointment_number;
+
+          // $("#modal").modal("show")
+          // $("#modal").modal({backdrop: 'static', keyboard: false})
+          $("#modal").modal({
+            backdrop: "static",
+            keyboard: false,
+            show: true,
+          });
         }
       });
+  }
+
+  onFileSelected(event) {
+    this.image = event.target.files[0];
+    this.appointForm.get('problem_Image').setValue(this.image)
+    //  console.log(this.image)
   }
 
   dateEventEmmiter(data) {
@@ -99,6 +126,4 @@ export class CreateAppointmentComponent implements OnInit {
     result += d.getMonth() + 1 + "/" + d.getDate() + "/" + d.getFullYear();
     return result;
   }
-
- 
 }
