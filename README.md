182f570b2621fa990c2dcf1d9bdfd0005880760a
ArunMaurya <arunmaurya2500@gmail.com>
2021-01-24 14:26:48 +0530

    some changes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# DjMobileGarage
+# DjMobileGarages
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.0.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.7.
 
 ## Development server
 
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/DjMobileGarage'),
+      dir: require('path').join(__dirname, './coverage/DjMobileGarages'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
