47440ccad45d4c0d79f0b30803c193b6585b2975
ArunMaurya <arunmaurya2500@gmail.com>
2021-02-26 17:39:01 +0530

    admin-pannel active link

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,26 +1,22 @@
 $(document).ready(function () {
+  $("#services_row").fadeOut();
+  $("#productRow").fadeOut();
 
-    $('#mobileImageBox').addClass('animate__animated animate__slideInLeft')
-    $('#companyNameBox').addClass('animate__animated animate__slideInRight')
-    
-    $('#services_row').fadeOut();
-    $('#productRow').fadeOut()
-    
-    $(window).scroll(()=>{
-        var scrollPosition = $(document).scrollTop();
-        
-        if(scrollPosition>100 && scrollPosition<500){
-            
-              $("#services_row").fadeIn("slow") ;
-              $(".imgBox").addClass('animate__animated animate__slideInLeft  ');
+  $("#mobileImageBox").addClass("animate__animated animate__slideInLeft");
+  $("#companyNameBox").addClass("animate__animated animate__slideInRight");
 
-              $(".details").addClass('animate__animated animate__slideInRight');
+  $(window).scroll(() => {
+    var scrollPosition = $(document).scrollTop();
 
-        }
-        if(scrollPosition>1000 && scrollPosition<1400){
-            $("#productRow").fadeIn("slow")
-            $("#productRow").addClass('animate__animated animate__flipInX')
-        }
-    })
-    
+    if (scrollPosition > 100 && scrollPosition < 500) {
+      $("#services_row").fadeIn("slow");
+      $(".imgBox").addClass("animate__animated animate__slideInLeft  ");
+
+      $(".details").addClass("animate__animated animate__slideInRight");
+    }
+    if (scrollPosition > 1000 && scrollPosition < 1400) {
+      $("#productRow").fadeIn("slow");
+      $("#productRow").addClass("animate__animated animate__flipInX");
+    }
+  });
 });
